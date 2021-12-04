4069b7b4e7db3b28eec5e421fee9bc30ed99aa78
Anton <areuta@mail.ru>
2021-12-04 09:12:39 +0300

    lesson2_5 EmployeeBook with Set

---
+++ java/course2/lesson2_5/homework/src/main/java/pro/sky/employeeset/emplsspring/EmplsSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeset.emplsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplsSpringApplication.class, args);
+	}
+
+}
---
+++ java/course2/lesson2_5/homework/src/test/java/pro/sky/employeeset/emplsspring/EmplsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeset.emplsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
