584fdf0eb406e06433b8cd532901e53d367ee783
Anton <areuta@mail.ru>
2021-12-06 18:09:08 +0300

    Employees with Map init

---
+++ java/course2/lesson2_7/homework/src/main/java/pro/sky/employeeswithmap/EmployeeswithmapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeswithmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeswithmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeswithmapApplication.class, args);
+	}
+
+}
