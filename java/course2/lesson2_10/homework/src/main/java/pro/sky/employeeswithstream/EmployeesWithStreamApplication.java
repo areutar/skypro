d0f2192bf55d67371a4a01cc4ecec4bd8c883524
Anton <areuta@mail.ru>
2021-12-09 08:08:13 +0300

    lesson2_10 Employees Libraries init

---
+++ java/course2/lesson2_10/homework/src/main/java/pro/sky/employeeswithstream/EmployeesWithStreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeswithstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesWithStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesWithStreamApplication.class, args);
+	}
+
+}
