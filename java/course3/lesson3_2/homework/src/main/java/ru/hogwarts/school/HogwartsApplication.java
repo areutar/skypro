72d396807dc1c437c5a9bd657a6fe95788ddfa94
Anton <areuta@mail.ru>
2022-01-22 15:51:24 +0300

    lesson3_2 hogwarts done

--- java/course3/lesson3_2/homework/src/main/java/ru/hogwarts/school/HogwartsApplication.java
+++ java/course3/lesson3_2/homework/src/main/java/ru/hogwarts/school/HogwartsApplication.java
@@ -1,11 +1,13 @@
 package ru.hogwarts.school;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeworkApplication {
+@OpenAPIDefinition
+public class HogwartsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HomeworkApplication.class, args);
+        SpringApplication.run(HogwartsApplication.class, args);
     }
 }
