52d0d60045633427c447c40a48941cc3f92911f1
Anton <areuta@mail.ru>
2022-04-10 04:07:16 +0300

    lesson4_2 DDL scripts fixed

--- java/course4/lesson4_2/homework/scripts423.sql
+++ java/course4/lesson4_2/homework/scripts423.sql
@@ -3,11 +3,9 @@ select student.name as student,
        faculty.name as faculty,
        student.avatar_id
 from student
-         inner join faculty on student.faculty_id = faculty.id;
+         join faculty on student.faculty_id = faculty.id;
 
 select student.name as student,
-       student.age,
-       faculty.name as faculty
+       student.age
 from student
-         inner join faculty on student.faculty_id = faculty.id
 where student.avatar_id is not null;
